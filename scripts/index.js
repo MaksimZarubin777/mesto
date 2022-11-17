449a9b254aa4507c41c50806f9a4f8f6ec539f7c
Maksim Zarubin <89940028777@ya.ru>
2022-11-17 19:49:33 +0400

    pr4-3

--- scripts/index.js
+++ scripts/index.js
@@ -5,21 +5,16 @@ let profileJob = document.querySelector('#profile-job');
 let myName = document.querySelector('.profile__text-title');
 let myJob = document.querySelector('.profile__text-subtitle');
 let closePopup = document.querySelector('.popup__button-close');
-let formElement = document.querySelector('.popup__container');
+let formElement = document.querySelector('#change-info');
 
 function toOpen () {
   popup.classList.add('popup_opened');
-  profileName.setAttribute('value', myName.textContent);
-  profileJob.setAttribute('value', myJob.textContent);
+  profileName.value = myName.textContent; 
+  profileJob.value = myJob.textContent;
 }
 
 function toClose() {
   popup.classList.remove('popup_opened');
-  // Функция именно для закрытия попапа, которая не изменяет введенное значение в инпут.
-  // если удалить две строчки ниже, то введя что-то в инпут, закрыв и открыв попап снова, 
-  // то в инпуте будет предыдущий текст. Не нашел другого решения :)
-  profileName.value = myName.textContent; 
-  profileJob.value = myJob.textContent;
 }
 
 function formSubmitHandler (evt) {
@@ -28,8 +23,7 @@ function formSubmitHandler (evt) {
     let jobInputValue = profileJob.value;
     myName.textContent = nameInputValue;
     myJob.textContent = jobInputValue;
-    // при сохранении используется просто удаление класса, т.к. у функции toClose немного другойфункционал
-    popup.classList.remove('popup_opened'); 
+    toClose();
 }
 
 openPopup.addEventListener('click', toOpen);
