08e387803e1533641b2586390f38c8a84ee0e4b0
Maksim Zarubin <89940028777@ya.ru>
2022-11-26 10:54:13 +0400

    feature/cards-add-from-js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// Попап
 let openPopup = document.querySelector('.profile__button-edit');
 let popup = document.querySelector('.popup');
 let profileName = document.querySelector('#profile-name'); 
@@ -7,16 +8,19 @@ let myJob = document.querySelector('.profile__text-subtitle');
 let closePopup = document.querySelector('.popup__button-close');
 let formElement = document.querySelector('#change-info');
 
+// функция открыть попап
 function toOpen () {
   popup.classList.add('popup_opened');
   profileName.value = myName.textContent; 
   profileJob.value = myJob.textContent;
 }
 
+// функция закрыть попап
 function toClose() {
   popup.classList.remove('popup_opened');
 }
 
+// функция кнопки сохранить
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     let nameInputValue = profileName.value;
@@ -29,3 +33,43 @@ function formSubmitHandler (evt) {
 openPopup.addEventListener('click', toOpen);
 closePopup.addEventListener('click', toClose);
 formElement.addEventListener('submit', formSubmitHandler); 
+
+// список карточек
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+
+// Добавление карточек из массива и теплейт
+for (i = 0; i < initialCards.length; i+=1) {
+  const elements = document.querySelector('.elements');
+  const cardTemplate = document.querySelector('#card');
+  const card = cardTemplate.content.cloneNode(true);
+  card.querySelector('.card__title').textContent = initialCards[i].name;
+  card.querySelector('.card__img').src = initialCards[i].link;
+  elements.append(card)
+}
+
